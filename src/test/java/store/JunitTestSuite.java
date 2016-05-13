c5e18b244a91ea93115f41b30231fb084557a214
bawar17 <bawar17@gmail.com>
2016-05-13 22:35:00 +0200

    created TestSuite

---
+++ src/test/java/store/JunitTestSuite.java
@@ -0,0 +1,13 @@
+package store;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestAddToCart.class, TestCategory.class, TestCheckOUt.class, TestHome.class, TestPro.class,
+		TestSearch.class })
+public class JunitTestSuite {
+
+	
+}
